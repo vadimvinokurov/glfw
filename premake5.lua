6e1145ecb5ac7c7716c457d128742c1e094fbf07
vadimvinokurov <vad9fly@gmail.com>
2024-02-04 12:39:58 +0300

    using common build option

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,16 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
+	staticruntime "on"
 	warnings "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	defines 
+	{ 
+		"_CRT_SECURE_NO_WARNINGS"
+	}
 
 	files
 	{
@@ -27,56 +32,7 @@ project "GLFW"
 		"src/window.c",
 	}
 
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_module.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
-	filter "system:macosx"
-		pic "On"
-
-		files
-		{
-			"src/cocoa_init.m",
-			"src/cocoa_monitor.m",
-			"src/cocoa_window.m",
-			"src/cocoa_joystick.m",
-			"src/cocoa_time.c",
-			"src/nsgl_context.m",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/osmesa_context.c",
-			"src/egl_context.c"
-		}
-
-		defines
-		{
-			"_GLFW_COCOA"
-		}
-
 	filter "system:windows"
-		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
 
 		files
@@ -95,19 +51,29 @@ project "GLFW"
 
 		defines 
 		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
+			"_GLFW_WIN32"
 		}
 
 	filter "configurations:Debug"
+		defines "VE_DEBUG"
 		runtime "Debug"
-		symbols "on"
-
+		optimize "Off"
+		symbols "On"
+		
 	filter "configurations:Release"
+		defines "VE_RELEASE"
 		runtime "Release"
-		optimize "speed"
-
-    filter { "configurations:Profile", "configurations:Retail" }
+		optimize "Speed"
+		symbols "Off"
+		
+	filter "configurations:Profile"
+		defines "VE_PROFILE"
 		runtime "Release"
-		optimize "speed"
-        symbols "off"
+		optimize "Speed"
+		symbols "Off"
+	
+	filter "configurations:Retail"
+		defines "VE_RETAIL"
+		runtime "Release"
+		optimize "Speed"
+		symbols "Off"
